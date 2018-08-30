550dfeb960553d8456e9e87de5bcde6a76cdd69a
jasminesautner <jasmineivysautner@gmail.com>
2018-08-30 14:09:34 -0700

    final project

---
+++ script.py
@@ -0,0 +1,22 @@
+adjective = raw_input("Enter an adjective: ")
+nationality = raw_input("Enter a nationality:")
+person = raw_input("Enter a person: ")
+noun = raw_input("Enter a noun: ")
+adjective1 = raw_input("Enter an adjective: ")
+noun1 = raw_input("Enter a noun: ")
+adjective2 = raw_input("Enter an adjective: ")
+adjective3 = raw_input("Enter an adjective: ")
+pluralnoun = raw_input("Enter a plural noun: ")
+noun1 = raw_input("Enter a noun: ")
+number = raw_input("Enter a number: ")
+shapes = raw_input("Enter a shape: ")
+food = raw_input("Enter a food: ")
+food2 = raw_input("Enter a food: ")
+number = raw_input("Enter a number: ")
+
+story_1 = "Pizza was invented by a {} {} chef named {}. To make pizza, you need to take a lump of {}, and make a thin, round {} {}. Then you cover it with {} sauce, {} cheese, and fresh chopped {}. When it is done, cut it into {} {}. Some kids like {} pizza the best, but my favorite is the {} pizza. If I could, I would eat pizza {} times a day!".format(adjective, nationality, person, noun, adjective, adjective1, noun1, adjective2, adjective3, pluralnoun, noun1, number, shapes, food, food2, number)
+
+print(story_1)
+
+
+
